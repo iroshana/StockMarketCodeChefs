13dcce20a71e1033a3d1cf455ea0a85a4d4b9520
Ravindu <ravinduiroshana123@live.com>
2018-06-20 01:17:24 +0530

    Global CORS configuration

--- StockMarketGameServerSide/StockMarketGame/src/main/java/codechef/stockmarket/codechef/StockMarketApplication.java
+++ StockMarketGameServerSide/StockMarketGame/src/main/java/codechef/stockmarket/codechef/StockMarketApplication.java
@@ -3,8 +3,12 @@ package codechef.stockmarket.codechef;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "codechef.stockmarket.*")
@@ -13,5 +17,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class StockMarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockMarketApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8070");
+            }
+        };
     }
 }
