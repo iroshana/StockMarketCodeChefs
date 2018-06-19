bb377cf5e3455e98bd4aa16f3cbbf2a07c9a5478
Ravindu <ravinduiroshana123@live.com>
2018-06-19 23:31:39 +0530

    Revert "config Allow cross origine "
    This reverts commit dbb665a9e368b952eaa99567a54d964d7bef8353.

--- StockMarketGameServerSide/StockMarketGame/src/main/java/codechef/stockmarket/codechef/StockMarketApplication.java
+++ StockMarketGameServerSide/StockMarketGame/src/main/java/codechef/stockmarket/codechef/StockMarketApplication.java
@@ -3,12 +3,8 @@ package codechef.stockmarket.codechef;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "codechef.stockmarket.*")
@@ -17,16 +13,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StockMarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(StockMarketApplication.class, args);
-    }
-    
-    @Bean
-    public WebMvcConfigurer corsConfigurer(){
-        return new WebMvcConfigurerAdapter(){
-            @Override
-            public void addCorsMappings(CorsRegistry registry){
-                registry.addMapping("/**")
-		.allowedOrigins("http://localhost:8070");
-            }
-        };
     }
 }
--- StockMarketGameServerSide/StockMarketGame/src/main/java/codechef/stockmarket/restcontroller/BankController.java
+++ StockMarketGameServerSide/StockMarketGame/src/main/java/codechef/stockmarket/restcontroller/BankController.java
@@ -14,7 +14,6 @@ import codechef.stockmarket.repository.TestRepositoryService;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +30,7 @@ public class BankController {
     @Autowired
     BankRepository BankRepository = null;
     
-    @GetMapping("/GetAllBanks")
+    @GetMapping("/GetAllBanks/")
     public List<BankViewModel> getNoteById() {
          List<BankViewModel> bankList = new ArrayList<>();
          List<Bank> banks = BankRepository.findAll();
